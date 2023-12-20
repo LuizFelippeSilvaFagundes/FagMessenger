224ddb75f4f94cd8679a3b6e833891e46a330012
LuizFelippe <luizfelippefagundes06@gmail.com>
2023-12-20 14:35:53 -0300

    Sincronização dos arquivos, implementado o update image para o firebase.

--- app/src/main/java/com/example/fagmessenger/LoginActivity.kt
+++ app/src/main/java/com/example/fagmessenger/LoginActivity.kt
@@ -1,11 +1,98 @@
 package com.example.fagmessenger
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fagmessenger.databinding.ActivityLoginBinding
+import com.example.fagmessenger.util.exibirMensagem
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthInvalidUserException
 
 class LoginActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
+
+    private lateinit var email: String
+    private lateinit var senha: String
+
+    private val firebaseAuth by lazy {
+        FirebaseAuth.getInstance()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+        inicializarEventosClick()
+        //firebaseAuth.signOut()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        verificarUsuarioLogado()
+    }
+
+    private fun verificarUsuarioLogado() {
+        val usuarioAtual = firebaseAuth.currentUser
+        if (usuarioAtual != null){
+            startActivity(
+                Intent(this, MainActivity::class.java))
+        }
+    }
+
+    private fun inicializarEventosClick() {
+        binding.textCadastro.setOnClickListener {
+            startActivity(Intent(this, CadastroActivity::class.java))
+        }
+        binding.btnLogin.setOnClickListener {
+            if (validarCampos()) {
+                logarUsuario()
+            }
+        }
+    }
+
+    private fun logarUsuario() {
+        firebaseAuth.signInWithEmailAndPassword(
+            email, senha
+        ).addOnSuccessListener {
+            exibirMensagem("Logado com sucesso")
+            startActivity(
+                Intent(this, MainActivity::class.java)
+            )
+        }.addOnFailureListener { erro ->
+            try {
+                throw erro
+            } catch (erroUsuarioInvalido: FirebaseAuthInvalidUserException) {
+                erroUsuarioInvalido.printStackTrace()
+                exibirMensagem("E-mail não cadastrado")
+            } catch (erroCredencialInvalida: FirebaseAuthInvalidCredentialsException) {
+                erroCredencialInvalida.printStackTrace()
+                exibirMensagem("Email ou senha estão incorretas")
+            }
+        }
+    }
+
+    private fun validarCampos(): Boolean {
+        email = binding.textEditEmail.text.toString()
+        senha = binding.textEditSenha.text.toString()
+
+        if (email.isNotEmpty()) {
+
+            binding.textInputLayoutLoginEmail.error = null
+            if (senha.isNotEmpty()) {
+                binding.textInputLayoutSenhaLogin.error = null
+                return true
+            }else{
+                binding.textInputLayoutSenhaLogin.error = "Preencha corretamente sua senha"
+                return false
+            }
+
+        }else {
+            binding.textInputLayoutLoginEmail.error = "Preencha seu email"
+            return false
+        }
     }
 }
+
--- app/src/main/java/com/example/fagmessenger/PerfilActivity.kt
+++ app/src/main/java/com/example/fagmessenger/PerfilActivity.kt
@@ -1,11 +1,133 @@
 package com.example.fagmessenger
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import com.example.fagmessenger.databinding.ActivityPerfilBinding
+import com.example.fagmessenger.util.exibirMensagem
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.userProfileChangeRequest
+import com.google.firebase.storage.FirebaseStorage
+
 
 class PerfilActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityPerfilBinding.inflate(layoutInflater)
+    }
+    private var temPermissaoCamera = false
+    private var temPermissaoGaleria = false
+
+    private val firebaseAuth by lazy {
+        FirebaseAuth.getInstance()
+    }
+    private val storage by lazy {
+        FirebaseStorage.getInstance()
+    }
+
+    private val gerenciadorGaleria = registerForActivityResult(
+        ActivityResultContracts.GetContent()
+    ) { uri ->
+        if (uri != null) {
+            binding.imagePerfil.setImageURI(uri)
+            uploadImagemStorage(uri)
+        } else {
+            exibirMensagem("Nenhuma imagem selecionada")
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_perfil)
+        setContentView(binding.root)
+        inicializarToolbar()
+        solicitarPermissoes()
+        inicializarEventosClique()
+    }
+    //update da imagem
+    private fun uploadImagemStorage(uri: Uri) {
+        val idUsuario = firebaseAuth.currentUser?.uid
+
+        if (idUsuario != null) {
+            storage
+                .getReference("fotos")
+                .child("usuarios")
+                .child(idUsuario)
+                .child("perfil.jpg")
+                .putFile(uri)
+                .addOnSuccessListener { task ->
+
+                    exibirMensagem("Sucesso ao fazer upload da imagem")
+
+                }
+                .addOnFailureListener {
+                    exibirMensagem("Erro ao fazer upload da imagem")
+                }
+        }
+    }
+
+
+    private fun inicializarEventosClique() {
+        binding.fabSelecionar.setOnClickListener {
+            if (temPermissaoGaleria) {
+                gerenciadorGaleria.launch("image/*")
+            } else {
+                exibirMensagem("Não tem permissão para acessar galeria")
+                solicitarPermissoes()
+            }
+        }
+    }
+
+    private fun solicitarPermissoes() {
+
+        //Verifico se usuário já tem permissão
+        temPermissaoCamera = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
+
+        temPermissaoGaleria = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.READ_MEDIA_IMAGES
+        ) == PackageManager.PERMISSION_GRANTED
+
+        //LISTA DE PERMISSÕES NEGADAS
+        val listaPermissoesNegadas = mutableListOf<String>()
+        if (!temPermissaoCamera)
+            listaPermissoesNegadas.add(Manifest.permission.CAMERA)
+        if (!temPermissaoGaleria)
+            listaPermissoesNegadas.add(Manifest.permission.READ_MEDIA_IMAGES)
+
+        if (listaPermissoesNegadas.isNotEmpty()) {
+
+            //Solicitar multiplas permissões
+            val gerenciadorPermissoes = registerForActivityResult(
+                ActivityResultContracts.RequestMultiplePermissions()
+            ) { permissoes ->
+
+                temPermissaoCamera = permissoes[Manifest.permission.CAMERA]
+                    ?: temPermissaoCamera
+
+                temPermissaoGaleria = permissoes[Manifest.permission.READ_MEDIA_IMAGES]
+                    ?: temPermissaoGaleria
+
+            }
+            gerenciadorPermissoes.launch(listaPermissoesNegadas.toTypedArray())
+
+        }
+
+    }
+
+
+    private fun inicializarToolbar() {
+        val toolbar = binding.includeToolbarPerfil.tbToolbar
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply {
+            title = "Editar perfil"
+            setDisplayHomeAsUpEnabled(true)
+        }
     }
 }
